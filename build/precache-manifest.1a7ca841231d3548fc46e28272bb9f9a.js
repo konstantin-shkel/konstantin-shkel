855704934a362c1f70cdfb44cd44aaaaca3153be
Konstantin Shkel <konstantin_shkel@epam.com>
2020-04-28 19:16:27 +0300

    updates

---
+++ build/precache-manifest.1a7ca841231d3548fc46e28272bb9f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79955b35915264d010be45f21f5efae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cba5c5444e6b4ca1be0c",
+    "url": "/static/css/main.23d80b12.chunk.css"
+  },
+  {
+    "revision": "9e720423f3b06f1bccbd",
+    "url": "/static/js/2.ca959a57.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca959a57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cba5c5444e6b4ca1be0c",
+    "url": "/static/js/main.5be1d59b.chunk.js"
+  },
+  {
+    "revision": "12306b35edf18e89ae2f",
+    "url": "/static/js/runtime-main.ad2f4fe2.js"
+  }
+]);
